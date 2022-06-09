04efea11b0b7510b9367e198790a7c81a0effb07
noahmj101 <200486844@student.georgianc.on.ca>
2022-06-09 02:31:15 -0400

    Tricky but im hopeful

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # This script will display basic system information!
 
-echo -e "Hostname:"
-hostname
-echo -e "Domainname:"
-dnsdomainname
-hostnamectl | grep "Operating"
-echo -e "IP Addresses:"
-ip addr | grep -E "192|fe80"
-echo -e "Root Filesystem Status:"
-df -H | grep "/dev/sda3"
+# Retrieves pc hostname
+hostname=$(hostname)
 
+# Retrieves fqdn
+fqdn=$(dnsdomainname)
+
+# Retrieves current IP address
+ip=$(ip route get 1.1.1.1 | grep 'src' | cut -b 41,42,43,44,45,46,47,48,49,50,51,52,53,54,55)
+
+# Retrieves free filesystem space
+filesystem=$(df -H | grep "/dev/sda3" | cut -b 23,24,25,26)
+
+lines="========================"
+echo -e " "
+echo -e "System info report for $hostname"
+echo -e "$lines"
+echo -e "FQDN: $fqdn"
+hostnamectl | grep "Operating"
+echo -e "IP Address: $ip"
+echo -e "Root Filesystem Free Space: $filesystem"
+echo -e "$lines"
+echo -e " "
