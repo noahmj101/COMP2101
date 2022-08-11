0ba4317115677b1eadd086b75c21dc95c436af63
noahmj101 <105834518+noahmj101@users.noreply.github.com>
2022-08-11 08:39:11 -0400

    Create Lab4.ps1

---
+++ powershell/Lab4.ps1
@@ -0,0 +1,23 @@
+function all-info {
+"System Hardware Description"
+"------------------------------------------"
+sys-desc
+"Operating System Info"
+"------------------------------------------"
+operating-system
+"Processor Info"
+"------------------------------------------"
+proc-desc
+"Memory Info"
+"------------------------------------------"
+get-meminfo
+"Disk Info"
+"------------------------------------------"
+get-diskinfo | format-table
+"Network Adapter Info"
+"------------------------------------------"
+get-ipinfo
+"Video Adapter Info"
+"------------------------------------------"
+get-videoinfo
+}
